a4dc17bc77b0eaf52765aeea6eed47f85451635a
Justin Heath <jheath@interstatelg.com>
2012-09-19 16:54:54 -0400

    Adding merge batch file

---
+++ tools/CreateMergedPlugin.bat
@@ -0,0 +1,2 @@
+copy ..\jquery-cookie\jquery.cookie.js+..\jquery.total-storage.js ..\jquery.total-storage.with.cookie.js
+jsmin < ..\jquery.total-storage.with.cookie.js > ..\jquery.total-storage.with.cookie.min.js
